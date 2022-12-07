9be9e6e46b1c9da04c0068a82c81a14e0e75e890
skyclouds2001 <skyclouds2001@163.com>
2022-12-07 11:58:49 +0800

    feat: add vue jsx support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
