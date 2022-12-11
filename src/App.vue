80607d453ed19a74bc64fc0d59b62330f1ecccca
skyclouds2001 <skyclouds2001@163.com>
2022-12-11 19:33:56 +0800

    feat: config element-plus component auto-import

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 </script>
 
 <template>
-  <router-view></router-view>
+  <el-config-provider :locale="zhCn">
+    <router-view></router-view>
+  </el-config-provider>
 </template>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import ElementPlus from 'unplugin-element-plus/vite'
 
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), ElementPlus()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
