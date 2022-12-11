3abc47293fc614ed07bcc9ee90ccad6ca17b42ac
skyclouds2001 <skyclouds2001@163.com>
2022-12-11 21:49:36 +0800

    feat: use pinia as store

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
-import '@/style.css'
 import App from '@/App.vue'
 import router from '@/router'
+import store from '@/store'
+import '@/style.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
