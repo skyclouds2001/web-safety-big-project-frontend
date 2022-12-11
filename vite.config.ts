dc47450ed796a095aae9ae1ae4bf83c63cd353c7
skyclouds2001 <skyclouds2001@163.com>
2022-12-11 23:05:03 +0800

    feat: use vite-eslint

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import ElementPlus from 'unplugin-element-plus/vite'
+import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
-  plugins: [vue(), vueJsx(), ElementPlus()],
+  plugins: [vue(), vueJsx(), ElementPlus(), eslint()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
