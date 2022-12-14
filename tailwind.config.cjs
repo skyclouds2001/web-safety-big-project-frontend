11cb04e6c6fea00848444b9ea5d5a3de1185e29e
skyclouds2001 <skyclouds2001@163.com>
2022-12-14 19:53:49 +0800

    feat: add tailwindcss config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx,vue}', './index.html'],
+  corePlugins: {
+    aspectRatio: false,
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/container-queries'),
+  ],
 }
