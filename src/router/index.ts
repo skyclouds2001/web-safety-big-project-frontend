766d2309e37892c143bc23ee0a671cab48a46744
skyclouds2001 <skyclouds2001@163.com>
2022-12-19 17:42:46 +0800

    feat: init page router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,5 +2,36 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 export default createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: async () => await import('@/views/BasePage.vue'),
+      children: [
+        {
+          path: '/login',
+          component: async () => await import('@/views/LoginPage.vue'),
+        },
+        {
+          path: '/register',
+          component: async () => await import('@/views/RegisterPage.vue'),
+        },
+        {
+          path: 'scenery-spot',
+          component: async () => await import('@/views/ScenerySpotManage.vue'),
+        },
+        {
+          path: 'ticket',
+          component: async () => await import('@/views/TicketManage.vue'),
+        },
+        {
+          path: '/order',
+          component: async () => await import('@/views/OrderManage.vue'),
+        },
+        {
+          path: '/user',
+          component: async () => await import('@/views/UserManage.vue'),
+        },
+      ],
+    },
+  ],
 })
