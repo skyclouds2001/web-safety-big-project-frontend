9741d1284cf11fa1338d3f1af56da0f411765125
skyclouds2001 <skyclouds2001@163.com>
2022-12-20 18:41:46 +0800

    feat: update ScenerySpotManage.vue

--- src/views/ScenerySpotManage.vue
+++ src/views/ScenerySpotManage.vue
@@ -1,9 +1,12 @@
 <template>
+  <div class="space">
+    <el-button type="primary" @click="addScenerySpot">添加景点</el-button>
+  </div>
   <el-table :data="scenerySpots" style="width: 1100px" stripe border empty-text="没有数据~" flexible>
     <el-table-column align="center" type="index" width="100" />
-    <el-table-column align="center" prop="name" label="景点名称" width="100" />
-    <el-table-column align="center" prop="desc" label="景点描述" width="300" />
-    <el-table-column align="center" prop="area" label="景点地点" width="200" />
+    <el-table-column align="center" prop="name" label="名称" width="100" />
+    <el-table-column align="center" prop="desc" label="描述" width="300" />
+    <el-table-column align="center" prop="area" label="地点" width="200" />
     <el-table-column align="center" prop="createTimestamp" label="创建时间" width="100" />
     <el-table-column align="center" prop="updateTimestamp" label="更新时间" width="100" />
     <el-table-column align="center" label="操作" width="200">
@@ -19,9 +22,11 @@
 import { ref, onMounted } from 'vue'
 import { ElTable, ElTableColumn, ElButton } from 'element-plus'
 import { getAllScenerySpot } from '@/api/scenery_spot'
+import titleStore from '@/store/title'
 import type ScenerySpot from '@/model/ScenerySpot'
 
 onMounted(async () => {
+  titleStore.updateTitle('景点管理')
   const res = await getAllScenerySpot()
   if (res.success) {
     scenerySpots.value = res.data
@@ -30,13 +35,24 @@ onMounted(async () => {
 
 const scenerySpots = ref<ScenerySpot[]>([])
 
+const addScenerySpot = () => {
+  console.log('add')
+}
+
 const editScenerySpot = (scenerySpot: ScenerySpot) => {
+  console.log('edit')
   console.log(scenerySpot)
 }
 
 const removeScenerySpot = (scenerySpot: ScenerySpot) => {
+  console.log('remove')
   console.log(scenerySpot)
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.space {
+  margin: 20px 0;
+  text-align: left;
+}
+</style>
