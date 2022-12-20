c55ef5096bcf45dbad6788d500653f039af34007
skyclouds2001 <skyclouds2001@163.com>
2022-12-20 17:45:02 +0800

    feat: update tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx,vue}', './index.html'],
-  corePlugins: {
-    aspectRatio: false,
-  },
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
