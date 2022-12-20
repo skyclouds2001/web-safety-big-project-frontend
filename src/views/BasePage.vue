d3aee031c9070b89e9ec5da2bd86ea6ad6e3c213
skyclouds2001 <skyclouds2001@163.com>
2022-12-20 16:42:48 +0800

    feat: use PageHeader & PageFooter component

--- src/views/BasePage.vue
+++ src/views/BasePage.vue
@@ -4,18 +4,24 @@
       <SideBar />
     </el-aside>
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>
+        <PageHeader />
+      </el-header>
       <el-main>
         <router-view></router-view>
       </el-main>
-      <el-footer>Footer</el-footer>
+      <el-footer>
+        <PageFooter />
+      </el-footer>
     </el-container>
   </el-container>
 </template>
 
 <script setup>
 import { ElContainer, ElMain, ElHeader, ElFooter, ElAside } from 'element-plus'
+import PageHeader from '@/components/PageHeader.vue'
 import SideBar from '@/components/SideBar.vue'
+import PageFooter from '@/components/PageFooter.vue'
 </script>
 
 <style scoped></style>
