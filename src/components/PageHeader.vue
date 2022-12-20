119aca14141f9b132cd4ea8eb62e2b04112aeaa7
skyclouds2001 <skyclouds2001@163.com>
2022-12-20 16:42:15 +0800

    feat: add PageHeader component

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-page-header title="返回" :content="title" @back="handleBack" />
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+import { storeToRefs } from 'pinia'
+import { ElPageHeader } from 'element-plus'
+import titleStore from '@/store/title'
+
+const router = useRouter()
+
+const { title } = storeToRefs(titleStore)
+
+const handleBack = () => {
+  console.log('back')
+  router.push('/')
+}
+</script>
+
+<style lang="less" scoped></style>
