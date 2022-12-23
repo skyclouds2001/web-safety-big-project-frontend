5fc25129910c49143673c38c1653be4327debbf9
skyclouds2001 <skyclouds2001@163.com>
2022-12-23 14:34:08 +0800

    feat: use config file

--- src/network/index.ts
+++ src/network/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import { SERVER_HOST } from '@/config'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: SERVER_HOST,
   timeout: 10000,
 })
 
