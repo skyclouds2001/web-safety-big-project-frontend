72f51d485160813a353fe345a4c5e549b6a29423
skyclouds2001 <skyclouds2001@163.com>
2022-12-24 18:16:29 +0800

    build: update stylelint config for support vue file

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  root: true,
+  customSyntax: 'postcss-html',
+  extends: ['stylelint-config-standard', 'stylelint-config-html', 'stylelint-config-prettier'],
+  rules: {},
+  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
 }
