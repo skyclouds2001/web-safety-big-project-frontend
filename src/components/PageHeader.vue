079d333745bb827da2a5417a072747a6a17fdc45
skyclouds2001 <skyclouds2001@163.com>
2022-12-25 16:04:40 +0800

    feat: update user

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,11 +1,28 @@
 <template>
-  <el-page-header class="header" title="返回" :content="title" @back="handleBack" />
+  <el-page-header class="header" title="返回" :content="title" @back="handleBack">
+    <template #extra>
+      <div class="flex justify-end" style="width: calc(100vw - 300px - 160px - 40px - 40px)">
+        <el-dropdown>
+          <el-icon :size="32" color="#999">
+            <UserFilled />
+          </el-icon>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item>重设密码</el-dropdown-item>
+              <el-dropdown-item>退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+      </div>
+    </template>
+  </el-page-header>
 </template>
 
 <script lang="ts" setup>
 import { useRouter } from 'vue-router'
 import { storeToRefs } from 'pinia'
-import { ElPageHeader } from 'element-plus'
+import { ElPageHeader, ElIcon, ElDropdown, ElDropdownMenu, ElDropdownItem } from 'element-plus'
+import { UserFilled } from '@element-plus/icons-vue'
 import titleStore from '@/store/title'
 
 const router = useRouter()
@@ -13,7 +30,6 @@ const router = useRouter()
 const { title } = storeToRefs(titleStore)
 
 const handleBack = () => {
-  console.log('back')
   router.push('/')
 }
 </script>
