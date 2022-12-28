b6290bbe61a01aa0e187fddbe8c9344cbef8614c
skyclouds2001 <skyclouds2001@163.com>
2022-12-28 15:42:10 +0800

    feat: let api return data directly

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -2,31 +2,35 @@ import instance from '@/network'
 import type RequestResult from '@/model/RequestResult'
 
 export const useLogin = async (phone: string, password: string): Promise<RequestResult<null>> =>
-  await instance.post(
-    '/admin/login',
-    {
-      phone,
-      password,
-    },
-    {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+  (
+    await instance.post(
+      '/admin/login',
+      {
+        phone,
+        password,
       },
-    }
-  )
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    )
+  ).data
 
 export const resetPassword = async (id: number, phone: string, oldPassword: string, newPassword: string): Promise<RequestResult<null>> =>
-  await instance.post(
-    '/admin/reset',
-    {
-      id,
-      phone,
-      oldPassword,
-      newPassword,
-    },
-    {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+  (
+    await instance.post(
+      '/admin/reset',
+      {
+        id,
+        phone,
+        oldPassword,
+        newPassword,
       },
-    }
-  )
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    )
+  ).data
