fab14b7deae471ad1278f09bcd8a06e796206228
skyclouds2001 <skyclouds2001@163.com>
2022-12-28 16:42:32 +0800

    build: config for support use tailwindcss with stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,14 @@ module.exports = {
   root: true,
   customSyntax: 'postcss-html',
   extends: ['stylelint-config-standard', 'stylelint-config-html', 'stylelint-config-prettier'],
-  rules: {},
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen', 'function', 'if', 'each', 'include', 'mixin'],
+      },
+    ],
+    'no-empty-source': false,
+  },
   ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
 }
